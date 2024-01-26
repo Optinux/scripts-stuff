cec0044df87a87626292c341940257309ea54bc7
Tobias Eglseder <tobias.eglseder2@gmail.com>
2024-01-26 03:43:14 +0100

    might work ⁉

--- install.ps1
+++ install.ps1
@@ -1,20 +1,12 @@
 # Script to install and configure ADDS, DHCP and DNS on Windows Server 2022
+# execute by running "irm https://raw.githubusercontent.com/Optinux/scripts-stuff/main/install.ps1 > C:\install.ps1 | iex"
 
 New-Item "C:\rcount.txt" -ItemType File -Value "0" # create lockfile
 $filePath = "C:\rcount.txt"
 $fileContent = Get-Content -Path $filePath
-if ($fileContent -eq "1") {
-    Write-Host "First Reboot succeeded, continuing with installation"
-    Remove-Item $filePath # remove lockfile
-    Install-ADDSForest -DomainName controller.local -InstallDNS     # setup Domain and Install DNS
-    Install-WindowsFeature DHCP -IncludeManagementTools # install DHCP
-    Add-DhcpServerInDC -DnsName controller.local -IPAddress 192.168.178.1 # authorize DHCP
-    Add-DhcpServerv4Scope -Name "scope1" -StartRange 192.168.178.1 -EndRange 192.168.178.254 -SubnetMask 255.255.255.0 # create DHCP scope
-    New-ADOrganizationalUnit -Name "FiSi" # create OU
-    New-ADUser -Name "Rainer Winkler" -GivenName "Rainer" -Surname "Winkler" -SamAccountName "rwinkler" -Path "OU=FiSi,DC=controller,DC=local" -AccountPassword (ConvertTo-SecureString "Pa$$w0rd" -AsPlainText -Force) -Enabled $true # create user
-    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0 # enable RDP
-    Add-LocalGroupMember -Group "Remote Desktop Users" -Member "rwinkler" # add user to RDP group
-} else {
+switch ($fileContent) 
+{
+  0 {
     Set-SConfig -AutoLaunch $false  # disable Sconfig auto launch
     Rename-Computer -NewName "winServer"    # rename computer
     Disable-NetAdapterBinding -Name Ethernet -ComponentID ms_tcpip6     # disable IPv6
@@ -23,11 +15,36 @@ if ($fileContent -eq "1") {
     Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name AUOptions -Value 4 # auto updates
     Install-WindowsFeature -name AD-Domain-Services -IncludeManagementTools   # install AD DS
 
-    # Restart and continue above
     Remove-Item $filePath # remove lockfile
     New-Item "C:\rcount.txt" -ItemType File -Value "1" # update lockfile
-    $scriptPath = $MyInvocation.MyCommand.Path
-    $runKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run"
+    $scriptPath = "C:\install.ps1" # path to script  
+    $runKeyPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Run" # set script to run on boot, only has to be done once
     Set-ItemProperty -Path $runKeyPath -Name "WinServerInstallScript" -Value $scriptPath
     shutdown /r /t 0
+    }
+
+  1 {
+    Write-Host "First Reboot succeeded, continuing with installation"
+    Remove-Item $filePath # remove lockfile
+    New-Item "C:\rcount.txt" -ItemType File -Value "2" # update lockfile
+
+    Install-ADDSForest -DomainName controller.local -InstallDNS -SafeModeAdministratorPassword "Pa$$w0rd" -Confirm    # setup Domain and Install DNS
+    shutdown /r /t 0
+    }
+  
+  2 {
+    Write-Host "Second Reboot succeeded, continuing with installation"
+    Remove-Item $filePath # remove lockfile
+
+    Install-WindowsFeature DHCP -IncludeManagementTools # install DHCP
+    Add-DhcpServerInDC -DnsName controller.local -IPAddress 192.168.178.1 # authorize DHCP
+    Add-DhcpServerv4Scope -Name "scope1" -StartRange 192.168.178.1 -EndRange 192.168.178.254 -SubnetMask 255.255.255.0 # create DHCP scope
+    New-ADOrganizationalUnit -Name "FiSi" # create OU
+    New-ADUser -Name "Rainer Winkler" -GivenName "Rainer" -Surname "Winkler" -SamAccountName "rwinkler" -Path "OU=FiSi,DC=controller,DC=local" -AccountPassword (ConvertTo-SecureString "Pa$$w0rd" -AsPlainText -Force) -Enabled $true # create user
+    Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server' -name "fDenyTSConnections" -value 0 # enable RDP
+    Add-LocalGroupMember -Group "Remote Desktop Users" -Member "rwinkler" # add user to RDP group
+    Write-Host "Installation finished!"
+
+    Remove-Item $scriptPath -Force # remove script
+    }
 }
